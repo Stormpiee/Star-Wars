7a455a3db079b5bcf58759772d8f0fa6761e6978
Storm <stormpiespam@gmail.com>
2021-04-29 10:31:55 +0200

    Update jedi.py

--- cogs/jedi.py
+++ cogs/jedi.py
@@ -2,6 +2,7 @@ import discord
 import json
 import os
 from discord.ext import commands 
+from open_acc import open_account, get_bank_data, get_user_data, open_inventory, get_inventory_data
 
 class _jedi(commands.Cog):
     def __init__(self, bot, *args, **kwargs):
@@ -20,8 +21,36 @@ class _jedi(commands.Cog):
         )
         msg = await ctx.author.send(embed=embed)
 
+    @commands.command(aliases = ["jedi_train", "jt"])
+    async def _jedi_training_ligthsaber(self, ctx):
+        role = discord.utils.get(ctx.message.guild.roles, name='Jedi')
+            if not role in ctx.author.roles:
+                await ctx.author.send("You are not a Jedi")
+                return
+
+        open_account(ctx.author)
+        open_inventory(ctx.author)
+
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+
+        await update_data(users, ctx.author)
+        await add_experience(users, ctx.author, 100)
+        await level_up(ctx, users, ctx.author, ctx.channel)
+
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+        await bot.process_commands(ctx)
+
+    async def update_data(users, user):
+        if not str(user.id) in users:
+            users[str(user.id)] = {}
+            users[str(user.id)]['experience'] = 0
+            users[str(user.id)]['force level'] = 1
+            users[str(user.id)]['lightsaber level'] = 1
+
 
 
 def setup(bot):
     bot.add_cog(_jedi(bot))
-    #print("loading jedi completed")
+    print("loading jedi completed")
